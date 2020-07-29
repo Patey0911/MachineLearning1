678bbe0d5cf80886082514ef9ecf9c1691f5703d
Patey0911 <44579714+Patey0911@users.noreply.github.com>
2020-07-29 11:58:07 +0300

    CC

---
+++ Tensorflow/Proiect.py
@@ -0,0 +1,10 @@
+import sklearn
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
