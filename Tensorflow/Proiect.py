243c4679203067ce796a7bfaaa43a95d02c1feb0
Patey0911 <44579714+Patey0911@users.noreply.github.com>
2020-07-29 12:46:15 +0300

    R

--- Tensorflow/Proiect.py
+++ Tensorflow/Proiect.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn.utils import shuffle
-
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 data = pd.read_csv("student-mat.csv", sep=";")
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
@@ -22,3 +24,9 @@ print(acc)
 
 print("Coefficient: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+    
