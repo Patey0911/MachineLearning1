7093f7d961e33fa346cc5c5983db7dbed53614c7
Patey0911 <44579714+Patey0911@users.noreply.github.com>
2020-08-05 11:44:30 +0300

    CC

--- Tensorflow/Proiect.py
+++ Tensorflow/Proiect.py
@@ -1,38 +1,22 @@
-import sklearn
-import pandas as pd
-import numpy as np
-from sklearn import linear_model
-from sklearn.utils import shuffle
-import matplotlib.pyplot as pyplot
-import pickle
-from matplotlib import style
-data = pd.read_csv("student-mat.csv", sep=";")
-data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+from PIL import ImageGrab
+import pyautogui
+import time
 
-predict = "G3"
+class Cordinates():
+    replayBtn = (494,470)
+    dinasaur = (264, 461)
 
-x = np.array(data.drop([predict], 1))
-y = np.array(data[predict])
+def restartGame():
+    pyautogui.click(Cordinates.replayBtn)
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+def pressSpace():
+    pyautogui.keyDown('space')
+    time.sleep(0.2)
+    print("Jump")
+    pyautogui.keyUp('space')
 
-"""linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
-
-with open("studentmodel.pickle","wb") as f:
-    pickle.dump(linear,f)"""
-
-pickle_in = open("studentmodel.pickle","rb")
-linear = pickle.load(pickle_in)
-
-print("Coefficient: \n", linear.coef_)
-print("Intercept: \n", linear.intercept_)
-
-predictions = linear.predict(x_test)
-
-for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
-    
+restartGame()
+while(True):
+    time.sleep(1)
+    pressSpace()
